4fafdc345e78077bc9750c4bbade2c6f45597861
thierryCz <zebizon13@gmail.com>
2022-09-26 07:44:18 +0200

    metastore ok

--- cmmetastore.sh
+++ cmmetastore.sh
@@ -4,4 +4,5 @@ kubectl create configmap metastore-cfg --dry-run=client \
 --from-file=hive_metastore/metastore-log4j2.properties \
 -o yaml | kubectl apply -f -
 
-kubectl create secret generic my-s3-keys --from-literal access-key=toto --from-literal secret-key=titi
+kubectl delete secret my-s3-keys
+kubectl create secret generic my-s3-keys --from-literal access-key=minio --from-literal secret-key=minio123
